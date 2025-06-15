5455204c0468c3f0eaa6d0f086d4bc52c4e5a5d9
zekibestia <ezequielor73@gmail.com>
2025-06-14 19:55:43 -0600

    error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
+import type { Configuration } from 'webpack';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config) => {
+  webpack: (config: Configuration) => {
+    config.resolve = config.resolve || {};
     config.resolve.fallback = {
       ...config.resolve.fallback,
       process: require.resolve('process/browser'),
